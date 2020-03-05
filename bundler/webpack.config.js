25e4eb74fe8ca1c490e4b2b7b42a3199ea26e7bf
nathangrigne <grigne.nathan@gmail.com>
2020-03-05 12:34:12 +0100

    :tada: Init

---
+++ bundler/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    devtool: 'source-map',
+    devServer:
+    {
+        contentBase: './dist',
+        open: true,
+        host: '0.0.0.0'
+    },
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: 
+    {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    plugins:
+    [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html'),
+            minify: true
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new CopyWebpackPlugin([ { from: 'static' } ]),
+    ],
+    module: 
+    {
+        rules:
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use:
+                [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.styl$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+            },
+            {
+                test: /\.(jpg|png|svg|gif)$/,
+                use: 
+                [
+                    {
+                        loader: 'file-loader',
+                        options: 
+                        { 
+                            name: '[name].[ext]',
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|otf)$/,
+                use: 
+                [
+                    {
+                        loader: 'file-loader',
+                        options: 
+                        {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
